d8e1c3dcf8a59dd61a7f81d80886777488de4bec
jiangyong <jiangyong@didichuxing.com>
2017-09-28 10:46:29 +0800

    add max and min cost

--- worker.go
+++ worker.go
@@ -78,22 +78,29 @@ func Result(result chan *result_t) {
 	var pre_succ_num int64
 	var pre_fail_num int64
 	var pre_cost int64
+	var max_cost, min_cost int64
 
 	var prestat = time.Now()
 	var preerror = time.Now()
+	max_cost = 0
+	min_cost = 1e20
 
 	//统计函数
 	stat := func(err error) {
 		cur := time.Now()
 		if cur.Sub(prestat).Seconds() >= 1 {
 			prestat = cur
-			log.Printf("total[%d] error[%d] avg[%.2f]ms",
+			log.Printf("total[%d] error[%d] max[%.2f] min[%.2f] avg[%.2f]ms",
 				succ_num-pre_succ_num, fail_num-pre_fail_num,
+				float64(max_cost)*float64(1000000),
+				float64(min_cost)*float64(1000000),
 				float64(cost-pre_cost)/(float64(succ_num-pre_succ_num)*float64(1000000)),
 			)
 			pre_succ_num = succ_num
 			pre_fail_num = fail_num
 			pre_cost = cost
+			max_cost = 0
+			min_cost = 1e20
 		}
 
 		if err != nil {
@@ -114,7 +121,14 @@ func Result(result chan *result_t) {
 			}
 			stat(nil)
 			succ_num += 1
-			cost += res.Cost.Nanoseconds()
+			tmp := res.Cost.Nanoseconds()
+			cost += tmp
+			if tmp > max_cost {
+				max_cost = tmp
+			}
+			if tmp < min_cost {
+				min_cost = tmp
+			}
 
 		}
 	}
